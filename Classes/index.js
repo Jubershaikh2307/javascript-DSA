fefe72cec155347ae51e8043959b59bbc50070f6
Juber Shaikh <jubershaikh2307@gmail.com>
2024-02-24 12:11:22 +0530

    Class Basic example

---
+++ Classes/index.js
@@ -0,0 +1,11 @@
+class Cookies {
+  constructor(color) {
+    this.color = color;
+  }
+}
+
+let cookiesOne = new Cookies("green");
+
+let cookiesTwo = new Cookies("blue");
+
+console.log(cookiesOne, cookiesTwo);
