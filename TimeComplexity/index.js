a9ea554f97339016d5ae76335b0dab15018e7f26
Juber Shaikh <jubershaikh2307@gmail.com>
2024-02-24 12:11:47 +0530

    Time complexity understanding

---
+++ TimeComplexity/index.js
@@ -0,0 +1,39 @@
+// function logTime(n) {
+//   for (let index = 0; index < n; index++) {
+//     console.log(index);
+//   }
+// }
+// logTime(3); // Time Complexity O(n)
+
+// function logTime(n) {
+//   for (let index = 0; index < n; index++) {
+//     for (let j = 0; j < n; j++) {
+//       console.log(index, j);
+//     }
+//   }
+// }
+// logTime(3); Time Complexity O(n^2)
+
+// function logTime(n) {
+//   for (let index = 0; index < n; index++) {
+//     for (let j = 0; j < n; j++) {
+//       for (let k = 0; k < n; k++) {
+//         console.log(index, j);
+//       }
+//     }
+//   }
+// }
+
+// logTime(3); // Time Complexity O(n^3)
+
+// Note: Nested loop increasing the power of time complexity increasing as per the loops
+
+// function logTime() {
+//   console.log("This is me");
+// }
+
+// logTime(); // Time complexity is O(1)
+
+// O(log n) - >  searching algorithm
+
+// O(n log n) - >Some sorting algorithm
