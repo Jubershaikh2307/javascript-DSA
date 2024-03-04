7b21da3f49700221bcf62feb4c3d276c97bf3151
Juber Shaikh <jubershaikh2307@gmail.com>
2024-03-04 10:25:39 +0530

    Push pop and print in stack

--- Stack/index.js
+++ Stack/index.js
@@ -1,3 +1,31 @@
-class Stack{
-    
+class Stack {
+  constructor() {
+    this.item = [];
+  }
+
+  push(value) {
+    this.item.push(value);
+  }
+
+  pop() {
+    return this.item.pop();
+  }
+
+  print() {
+    for (let i = 0; i < this.item.length; i++) {
+      console.log(this.item[i]);
+    }
+  }
 }
+
+let stack = new Stack();
+
+stack.push(1);
+
+stack.push(2);
+
+stack.push(3);
+
+stack.pop();
+
+stack.print();
